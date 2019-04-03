6dda776f8f0ce117a562e1703405ba7cfde6d881
Michael Urban <michaelurban@Michaels-MacBook-Pro-2.local>
2019-04-03 14:57:20 +0200

    add back tracking

--- src/Board.java
+++ src/Board.java
@@ -3,6 +3,12 @@ import java.util.ArrayList;
 public class Board {
     private int size;
 
+    private int forward_checking_returns = 0;
+    private int back_tracking_returns = 0;
+
+    private int forward_checking_set_queen = 0;
+    private int back_tracking_set_queen = 0;
+
     public Board(int size) {
         this.size = size;
     }
@@ -15,24 +21,45 @@ public class Board {
 
     private int previousCol = 0;
 
-    public void backTracking() {
+    public void forwardChecking() {
         Queen temp;
         for (int row = 0; row < size; row++) {
             added = false;
 
             for (int col = previousCol; col < size; col++) {
                 temp = new Queen(row, col);
-                if (!checkColission(temp)) {
+                if (!checkCollision(temp)) {
                     queens.add(temp);
                     added = true;
+                    forward_checking_set_queen += 1;
                     break;
                 }
             }
             if (!added) {
+                forward_checking_returns += 1;
                 temp = queens.remove(queens.size() - 1);
                 previousCol = temp.getCol() + 1;
                 row = temp.getRow() - 1;
-            }else{
+            } else {
+                previousCol = 0;
+            }
+        }
+    }
+
+    public void backTracking() {
+        Queen temp;
+        int previousCol = 0;
+        for (int row = 0; row < size; row++) {
+            temp = new Queen(row, previousCol);
+            queens.add(temp);
+            back_tracking_set_queen += 1;
+
+            if (checkCollision(queens.get(queens.size() - 1))) {
+                temp = queens.remove(queens.size() - 1);
+                row = temp.getRow() - 1;
+                previousCol = temp.getCol() + 1;
+                back_tracking_returns += 1;
+            } else {
                 previousCol = 0;
             }
         }
@@ -44,7 +71,7 @@ public class Board {
         // print header
         System.out.print("  |");
         for (int col = 0; col < size; col++) {
-                System.out.print( " " + col + " |");
+            System.out.print(" " + col + " |");
         }
         System.out.println();
         for (int col = 0; col < size; col++) {
@@ -61,9 +88,9 @@ public class Board {
 
             for (int col = 0; col < size; col++) {
                 // print queen position, if queen is on the position print X otherwise print white space
-                if (temp.getCol() == col){
+                if (temp.getCol() == col) {
                     System.out.print("| X ");
-                }else{
+                } else {
                     System.out.print("|   ");
                 }
             }
@@ -77,16 +104,17 @@ public class Board {
         }
     }
 
-    private boolean checkColission(Queen queen) {
-        if (checkDiagonals(queen) || checkHorizontal(queen) || checkVerical(queen)) {
-            return true;
+    // if there isn't a collision return false
+    private boolean checkCollision(Queen queen) {
+        if (!checkVertical(queen) && !checkHorizontal(queen) && !checkDiagonals(queen)) {
+            return false;
         }
-        return false;
+        return true;
     }
 
     private boolean checkDiagonals(Queen queen) {
         for (Queen q : queens) {
-            if (Math.abs(q.getRow() - queen.getRow()) == Math.abs(q.getCol() - queen.getCol())) {
+            if (Math.abs(q.getRow() - queen.getRow()) == Math.abs(q.getCol() - queen.getCol()) && q != queen) {
                 return true;
             }
         }
@@ -95,19 +123,31 @@ public class Board {
 
     private boolean checkHorizontal(Queen queen) {
         for (Queen q : queens) {
-            if (q.getRow() == queen.getRow()) {
+            if (q.getRow() == queen.getRow() && q != queen) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean checkVerical(Queen queen) {
+    private boolean checkVertical(Queen queen) {
         for (Queen q : queens) {
-            if (q.getCol() == queen.getCol()) {
+            if (q.getCol() == queen.getCol() && q != queen) {
                 return true;
             }
         }
         return false;
+    }
+
+    public void clearBoard(){
+        queens.clear();
+    }
+
+
+    public String getStatistic(){
+        return "forward checking set queen " + forward_checking_set_queen +
+                "\nforward checking returns " + forward_checking_returns +
+                "\nback tracking set queen " + back_tracking_set_queen +
+                "\nback tracking returns " + back_tracking_returns;
     }
 }
