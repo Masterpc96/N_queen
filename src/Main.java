31cf2817c04006e0e2570e29c2bbc821b9f108a6
Michael Urban <michaelurban@Michaels-MacBook-Pro-2.local>
2019-04-03 14:57:04 +0200

    call backtracking

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String args[]) {
-        Board board = new Board(7);
+        Board board = new Board(5);
+        board.forwardChecking();
+        board.clearBoard();
         board.backTracking();
-        board.print();
+        System.out.println(board.getStatistic());
     }
 }
